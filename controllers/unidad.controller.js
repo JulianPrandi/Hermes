344255c09487a09b4e26d9f1f383569cc7a0ce37
Neuro <alucard.j.pr@gmail.com>
2021-11-02 19:20:53 -0300

    no se puede crear un nuevo documento en la base ya que no se reconoce el metodo para crear un nuevo modelo.

--- controllers/unidad.controller.js
+++ controllers/unidad.controller.js
@@ -1,10 +1,45 @@
 //import Unidad from "../model/Unidad";
-
+const Unidad = require("../model/Unidad");
 //export const renderNoteForm = (req, res) => {
 //  res.render("notes/new-note");
 //};
 
 const createNewUnit = async (req, res) => {
-  res.send("<h1><center>Crea la unidad</center></h1>");
+  const { unitType, name, Lv, attributes, powers, gear } = req.body;
+
+  const errors = [];
+  if (!unitType) {
+    errors.push("Please insert a Unit Type.");
+  }
+  if (!name) {
+    errors.push("Please insert a Name for this unite");
+  }
+  if (!Lv) {
+    errors.push("Please insert a Lv from this unit");
+  }
+  if (!attributes) {
+    errors.push("Please complete the attributes for this unit");
+  }
+  if (errors.length > 0) {
+    let error = "<ul>";
+    errors.forEach((element) => (error = error + `<li>${element}</li>`));
+    error = error + "</ul>";
+    res.send(error);
+  } else {
+    console.log("req.body: ", req.body);
+    res.send("<h1>Exito</h1>");
+    let newUnit = new Unidad({
+      unitType,
+      name,
+      Lv,
+      attributes,
+      powers,
+      gear,
+    });
+    //newUnit.user = req.user.id;
+    await newUnit.save();
+    //req.flash("success_msg", "Note Added Successfully");
+    res.redirect("/");
+  }
 };
 module.exports = createNewUnit;
